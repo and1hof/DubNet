0ddab37f69262b1019b6d0279ff0b903c5cc1efd
Andrew Hoffman <andhofmt1993@gmail.com>
2015-11-05 03:24:54 +0000

    Added email REGEX checking to the User model. We should also check REGEX in the JavaScript form later on for added security.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
          validates_uniqueness_of :username
          # we must have a username to create an entry
          validates :username, :presence => true
+         validates_format_of :email, with: /(\w+)(@uw.edu)|(\w+)(@u.washington.edu)/, on: :create
 end
